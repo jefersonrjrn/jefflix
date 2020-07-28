1805159e74af79eaff30297740ee6734a71e579a
Jeferson Oliveira <jeferson.rjrn@gmail.com>
2020-07-28 11:32:19 -0300

    Criação das páginas de cadatro

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Desafio
+const Pagina404 = () => (<div>Página 404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // BrowserRouter: Tudo que tiver dentro é que será roteado
+  // Se não usar o "exact", vai ser encontrado a "/" e ele não ira proseguir na rota.
+  // Se eu não passar o 'path' ele vai executar o component independente da rota digita no browser
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
 const AppWrapper = styled.div`
   background: #141414;
 `
 
-function App() {
+function Home() {
   return (
     <AppWrapper >
       <Menu />
@@ -51,4 +51,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
